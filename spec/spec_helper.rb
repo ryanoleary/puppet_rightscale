2ed20c6055c8eb258e00c7684fa3d50c9f76b974
Matt Wise <matt@nextdoor.com>
2014-05-13 17:44:37 -0700

    Completely disable the SimpleCov module until it works in 1.8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,17 @@
 require 'rubygems'
-require 'simplecov'
-require 'simplecov-csv'
+#require 'simplecov'
+#require 'simplecov-csv'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 # Configure the coverage reporter
-SimpleCov.start do
-  add_filter '/spec/'
-end
-SimpleCov.refuse_coverage_drop
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::CSVFormatter,
-]
+#SimpleCov.start do
+#  add_filter '/spec/'
+#end
+#SimpleCov.refuse_coverage_drop
+#SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+#  SimpleCov::Formatter::HTMLFormatter,
+#  SimpleCov::Formatter::CSVFormatter,
+#]
 
 # TODO(mwise): Figure out how to exceute this only in Ruby 1.9+,
 # it fails in Ruby 1.8.7.
