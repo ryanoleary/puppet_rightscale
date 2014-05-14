62ce0e093d322482320b75a8e8c28bf159f55a45
Matt Wise <matt@nextdoor.com>
2014-05-13 17:31:29 -0700

    Allow 'rs_tag --list' to fail gracefully.
    If the prefetch of tags fails, the worst case is that Puppet
    re-tags the host with its own tags -- which is fine. This allows
    the execution of 'rs_tag --list' to fail gracefully, and the
    module still works.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,10 @@ SimpleCov.start do
   add_filter '/spec/'
 end
 SimpleCov.refuse_coverage_drop
-SimpleCov.formatter = SimpleCov::Formatter::CSVFormatter
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::CSVFormatter,
+]
 SimpleCov.at_exit do
   SimpleCov.result.format!
 end
